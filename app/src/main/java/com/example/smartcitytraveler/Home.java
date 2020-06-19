1fcc0304af120ae285bf8351b4d489a5137bdb36
dellahi <mohamed_abdellahi.el_waghf.etu@univ-lemans.fr>
2020-06-19 22:14:21 +0200

    google Maps + Places APIs

--- app/src/main/java/com/example/smartcitytraveler/Home.java
+++ app/src/main/java/com/example/smartcitytraveler/Home.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class Home extends AppCompatActivity {
 
     private Button commencer;
     private Home activity;
+    private static final String TAG = "Home";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
 
     @Override
@@ -30,23 +38,7 @@ public class Home extends AppCompatActivity {
         commencer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Custompopup popup = new Custompopup(activity);
-                popup.getAnnuler().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        popup.dismiss();
-                    }
-                });
-                popup.getValider().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent otheractivity = new Intent(getApplicationContext(), History.class);
-                        startActivity(otheractivity);
-                        finish();
-                        popup.dismiss();
-                    }
-                });
-                popup.build();
+                init();
             }
         });
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -54,7 +46,46 @@ public class Home extends AppCompatActivity {
         //toolbar.setTitle("Home");
         //toolbar.inflateMenu(R.menu.menu);
     }
+    private void init(){
+        final Custompopup popup = new Custompopup(activity);
+        popup.getAnnuler().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popup.dismiss();
+            }
+        });
+        popup.getValider().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(otherActivity);
+                finish();
+                popup.dismiss();
+            }
+        });
+        popup.build();
+    }
 
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(Home.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(Home.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
--- app/src/main/java/com/example/smartcitytraveler/MainActivity.java
+++ app/src/main/java/com/example/smartcitytraveler/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smartcitytraveler;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,16 +11,35 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button play;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("tag1","create");
         setContentView(R.layout.activity_main);
+        init();
+       /* if(isServicesOK()){
+            init();
+        }*/
+    }
 
+    /*public boolean OnCreateOptionsMenu(Menu menu){
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }*/
+    private void init(){
         play = this.findViewById(R.id.button);
         play.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -29,11 +49,25 @@ public class MainActivity extends AppCompatActivity {
             }
         } );
     }
+/*
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
 
-    /*public boolean OnCreateOptionsMenu(Menu menu){
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
 
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
-        return true;
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }*/
 }
